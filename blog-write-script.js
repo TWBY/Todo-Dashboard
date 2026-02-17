6ca025add5c1e15681b0995b493933161c353b23
阮柏燁 <willy633526@gmail.com>
2026-02-17 23:47:00 +0800

    feat: 新增博客寫作輔助腳本

---
+++ blog-write-script.js
@@ -0,0 +1,5 @@
+const fs=require("fs");
+const S=String.fromCharCode(124);
+const rows=[];
+rows.push("> **TL;DR**");
+rows.push("> 透過作者十年的親身經歷，理解 AI Coding 工具如何在三次典範轉移中加速進化，以及為什麼下一個「汽車級」產品會比你想的更快到來。");
